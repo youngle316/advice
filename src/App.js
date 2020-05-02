87f788c9c021223602a866eee615db7489ef1db0
youngle316 <youngle316@gmail.com>
2020-05-02 16:58:59 +0800

    add: advice project

--- src/App.js
+++ src/App.js
@@ -1,8 +1,41 @@
 import React from 'react';
+import axios from 'axios';
 import './App.css';
 
-function App() {
-  return <div className="App">Hello</div>;
+class App extends React.PureComponent {
+  constructor(props) {
+    super(props);
+    this.state = {
+      adviceText: '',
+    };
+  }
+
+  componentDidMount() {
+    this.fetchAdvice();
+  }
+
+  fetchAdvice = () => {
+    axios.get('https://api.adviceslip.com/advice').then((response) => {
+      const { advice } = response.data.slip;
+      this.setState({
+        adviceText: advice,
+      });
+    });
+  };
+
+  render() {
+    const { adviceText } = this.state;
+    return (
+      <div className="app">
+        <div className="card">
+          <h1 className="heading">{adviceText}</h1>
+          <button className="button" type="button" onClick={this.fetchAdvice}>
+            <span>Give me advice!</span>
+          </button>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
